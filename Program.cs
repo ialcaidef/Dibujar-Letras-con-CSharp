08d6447b27bbc7dbc3475281c196621d32c5a772
Ino <71455140+ialcaidef@users.noreply.github.com>
2020-10-09 21:12:21 +0200

    Add files via upload
    Dibujar letras siguiendo un patrón. En este caso la U y la X

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace letra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pintarU();
+            pintarX();
+        }
+
+        static char[,] pintarU()
+        {
+            
+            char[,] letraU = new char[7,7] { {'*',' ',' ',' ',' ',' ','*'},
+                                             {'*',' ',' ',' ',' ',' ','*'},
+                                             {'*',' ',' ',' ',' ',' ','*'},
+                                             {'*',' ',' ',' ',' ',' ','*'},
+                                             {'*',' ',' ',' ',' ',' ','*'},
+                                             {'*',' ',' ',' ',' ',' ','*'},
+                                             {'*','*','*','*','*','*','*'}};
+            
+            for (int row = 0; row < 7; row++) 
+            {
+                for (int col = 0; col < 7; col++)
+                {
+                    Console.Write(letraU[row,col]);
+                }   
+                Console.WriteLine();
+            }
+
+            return letraU;    
+        }
+
+        static char[,] pintarX()
+        {
+            char[,] letraX = new char[7,7] { {'*',' ',' ',' ',' ',' ','*'},
+                                             {' ','*',' ',' ',' ','*',' '},
+                                             {' ',' ','*',' ','*',' ',' '},
+                                             {' ',' ',' ','*',' ',' ',' '},
+                                             {' ',' ','*',' ','*',' ',' '},
+                                             {' ','*',' ',' ',' ','*',' '},
+                                             {'*',' ',' ',' ',' ',' ','*'}};
+            
+            for (int row = 0; row < 7; row++) 
+            {
+                for (int col = 0; col < 7; col++)
+                {
+                    Console.Write(letraX[row,col]);
+                }   
+                Console.WriteLine();
+            }
+
+            return letraX;  
+
+        }
+    }
+}
